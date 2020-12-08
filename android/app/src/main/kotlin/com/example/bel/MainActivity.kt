8de47a68d637fbccd47d049387df3adb81d565ed
WibiSa <rwibisa@gmail.com>
2020-12-08 11:35:15 +0800

    fixing fitur bg service

--- android/app/src/main/kotlin/com/example/bel/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/bel/MainActivity.kt
@@ -5,53 +5,65 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
-import android.os.IBinder
-import android.os.Messenger
+import android.os.*
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
-    val CHANNEL = "backgroundServices.channel"
-    val KEY_NATIVE = "BackGroundService"
-    private var mServices: Messenger? = null
-    private lateinit var mBoundServicesIntent: Intent
-    private var mServicesBound = false
-    private val mServicesConnection = object: ServiceConnection{
-        override fun onServiceDisconnected(p0: ComponentName?) {
-            TODO("Not yet implemented")
-            mServices = null
-            mServicesBound = false
+class MainActivity : FlutterActivity() {
+    private var service: Messenger? = null
+    private lateinit var boundServiceIntent: Intent
+    private var serviceBound = false
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(p0: ComponentName, p1: IBinder) {
+            service = Messenger(p1)
+            serviceBound = true
         }
 
-        override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
-            TODO("Not yet implemented")
-            mServices = Messenger(p1)
-            mServicesBound = true
+        override fun onServiceDisconnected(p0: ComponentName) {
+            service = null
+            serviceBound = false
         }
-
     }
 
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger,CHANNEL).setMethodCallHandler{
-            call, result ->
-            if (call.method == KEY_NATIVE){
-                mBoundServicesIntent = Intent(this@MainActivity,BackGroundServices::class.java)
-                mBoundServicesIntent.action = BackGroundServices.ACTION_CREATED
-                startService(mBoundServicesIntent)
-                bindService(mBoundServicesIntent,mServicesConnection,Context.BIND_AUTO_CREATE)
-                result.success("berhasil")
-            }
-        }
+    companion object {
+        private const val CHANNEL = "com.embundev.bel/bgServiceWithNotif"
+        private const val METHOD = "getBgServiceWithNotif"
+    }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        boundServiceIntent = Intent(this@MainActivity, NativeService::class.java)
+        boundServiceIntent.action = NativeService.ACTION_CREATE
+
+        startService(boundServiceIntent)
+        bindService(boundServiceIntent, serviceConnection, Context.BIND_AUTO_CREATE)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        unbindService(mServicesConnection)
-        mBoundServicesIntent.action = BackGroundServices.ACTION_DESTROY
-        startService(mBoundServicesIntent)
+        unbindService(serviceConnection)
+        boundServiceIntent.action = NativeService.ACTION_DESTROY
+        startService(boundServiceIntent)
+    }
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+
+            if (call.method == METHOD) {
+                //
+                if (!serviceBound) return@setMethodCallHandler
+                try {
+                    service?.send(Message.obtain(null, NativeService.START, 0, 0))
+                    result.success("background service active")
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
