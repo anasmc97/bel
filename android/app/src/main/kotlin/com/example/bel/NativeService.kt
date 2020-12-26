913aaa3157be2b8288ab3016b0727222572e89ef
WibiSa <rwibisa@gmail.com>
2020-12-26 09:48:55 +0800

    menambahkan ic alarm untuk notifikasi bg service

--- android/app/src/main/kotlin/com/example/bel/NativeService.kt
+++ android/app/src/main/kotlin/com/example/bel/NativeService.kt
@@ -12,23 +12,27 @@ import java.lang.ref.WeakReference
 
 class NativeService : Service(), NativeServiceCallback {
 
-    companion object{
+    companion object {
         const val ACTION_CREATE = "nativeService.create"
         const val ACTION_DESTROY = "nativeService.destroy"
         const val START = 1
         const val STOP = 0
+        const val CHANNEL_NAME = "channel bel"
+        const val NOTIFICATION_ID = 95
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         val action = intent.action
         if (action != null) {
-            when(action) {
-                ACTION_CREATE -> {}
+            when (action) {
+                ACTION_CREATE -> {
+                }
                 ACTION_DESTROY -> {
                     stopNotif()
                     stopSelf()
                 }
-                else -> {}
+                else -> {
+                }
             }
         }
         return flags
@@ -59,8 +63,7 @@ class NativeService : Service(), NativeServiceCallback {
     }
 
     private fun showNotif() {
-        val CHANNEL_NAME = "channel bel"
-        val NOTIFICATION_ID = 95
+
 
         val notificationIntent = Intent(this, MainActivity::class.java)
         notificationIntent.flags = Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT
@@ -69,9 +72,9 @@ class NativeService : Service(), NativeServiceCallback {
 
         val notification = NotificationCompat.Builder(this, CHANNEL_NAME)
                 .setContentTitle("Bel Sekolah")
-                .setContentText("Running...")
+                .setContentText("Sedang berjalan...")
                 .setContentIntent(pendingIntent)
-                .setSmallIcon(R.drawable.launch_background)
+                .setSmallIcon(R.drawable.ic_access_alarm)
                 .build()
 
         mCreateChannel(CHANNEL_NAME)
